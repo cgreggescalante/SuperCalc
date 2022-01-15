34fd1e8755ce74acbf612bf85c9fb0ca53dd50b9
Conor <conor@johngregg.org>
2022-01-15 11:44:24 -0600

    Added adjustable x-axis bounds for GraphingCalculator

--- src/main/java/Calculator/GraphingCalculator.java
+++ src/main/java/Calculator/GraphingCalculator.java
@@ -16,12 +16,17 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class GraphingCalculator implements View {
+    private static final int FUNCTION_RESOLUTION = 100;
 
     private JTextField equationTextField;
     private JButton displayButton;
     private JPanel graphPanel;
     private JPanel root;
     private JButton menuButton;
+    private JTextField xMinTextField;
+    private JTextField xMaxTextField;
+    private JTextField yMaxTextField;
+    private JTextField yMinTextField;
 
     private JFreeChart chart;
 
@@ -64,16 +69,20 @@ public class GraphingCalculator implements View {
     private XYDataset createDataset() {
         Expression expression = new Expression(equationTextField.getText());
 
+        int x_min = Integer.parseInt(xMinTextField.getText());
+        int x_max = Integer.parseInt(xMaxTextField.getText());
+
+        int points = (x_max - x_min) * FUNCTION_RESOLUTION + 1;
         DefaultXYDataset ds = new DefaultXYDataset();
 
-        double[][] data = new double[2][1000];
+        double[][] data = new double[2][points];
 
         Map<String, Double> variables = new HashMap<>();
         variables.put("x", 0d);
 
-        for (int x = 0; x < 1000; x++) {
-            data[0][x] = x / 100d;
-            variables.put("x", x / 100d);
+        for (int x = 0; x < points; x++) {
+            data[0][x] = ((double) x) / FUNCTION_RESOLUTION + x_min;
+            variables.put("x", ((double) x) / FUNCTION_RESOLUTION + x_min);
             data[1][x] = expression.evaluate(variables);
         }
 
